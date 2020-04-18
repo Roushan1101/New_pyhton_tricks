fc0ebe31ab4bbcd11847aad6e43d36345d66d2ba
Roushan kumar <63840285+Roushan1101@users.noreply.github.com>
2020-04-18 06:00:09 +0530

    Add files via upload

---
+++ input methods easy_way.py
@@ -0,0 +1,17 @@
+arr=[7,9,9,9,9,9,9,9,0]
+print(set(arr))                  #set removes all the duplicates
+m,n=tuple(map(int,input().split()))  # great merhod
+print(m,n)
+d=input().split()   #if not set it is list by default
+print("first element: ",d[0])
+
+
+for i in range(101,-1,-2):  #loop 3 arguments
+ print(i,end=" ")
+
+
+#seperarion
+word="kasdhfkjaskudf"
+b=3
+h=[word[i:i+b] for i in range(0, len(word), b)]
+print(h)
